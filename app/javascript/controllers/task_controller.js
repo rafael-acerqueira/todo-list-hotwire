b4c8606a9f9a2fa61bec5ea33eb7b5a7b55eeae5
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-11-15 18:35:58 -0300

    Add check and uncheck functionality

---
+++ app/javascript/controllers/task_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="task"
+export default class extends Controller {
+
+  static targets = ['checkbox']
+
+  connect() {
+  }
+
+  toggle() {
+    this.checkboxTarget.closest('form').requestSubmit();
+  }
+}
