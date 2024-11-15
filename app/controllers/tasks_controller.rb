dbc89ecef9bb665fec0af465940c27480919fde4
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-11-15 18:24:51 -0300

    Add Turbo Stream to Destroy action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -60,7 +60,11 @@ class TasksController < ApplicationController
   # DELETE /tasks/1
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "Task was successfully destroyed.", status: :see_other
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@task)}_li") }
+      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+    end
   end
 
   private
